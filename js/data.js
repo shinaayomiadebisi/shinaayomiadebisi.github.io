9fe1461287442e6fdf0fae3a1c4c41f5572dc8ae
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2025-12-27 03:14:12 -0800

    updated data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,33 @@
 const projects = [
+  {
+    title: "School Management Application",
+    description: "",
+    githubLink: "",
+    liveUrl: "",
+    tools: [],
+  },
+  {
+    title: "MERN Real Estate",
+    description: "",
+    githubLink: "",
+    liveUrl: "",
+    tools: [],
+  },
   { title: "", description: "", githubLink: "", liveUrl: "", tools: [] },
   { title: "", description: "", githubLink: "", liveUrl: "", tools: [] },
 ];
 
-export { projects };
+const certifications = [
+  {
+    title: "C# Beginner Certification",
+    certificateSiteUrl:
+      "https://www.udemy.com/certificate/UC-e8d17b9e-7b38-4a09-8cee-ba6986b08616/",
+  },
+  {
+    title: "Fullstack MERN Real Estate",
+    certificateSiteUrl:
+      "https://www.udemy.com/certificate/UC-37c0f482-4aca-4684-b31c-2685df5ebfeb/",
+  },
+];
+
+export { projects, certifications };
