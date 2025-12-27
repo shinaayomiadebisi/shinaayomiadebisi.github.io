9bc751340501f547f34202ea0c9266256bcec012
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2025-12-27 03:13:33 -0800

    updated app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let footer = document.getElementsByTagName("footer")[0];
+
+let footerDate = footer.getElementsByClassName("footerDate")[0];
+footerDate.innerHTML = new Date().getFullYear();
+
+// document.addEventListener("DOMContentLoaded", () => {});
