7be15da5ea0eca31cb010189aa2fd580654da630
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2025-12-29 01:54:34 -0800

    update data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
+import { certificationsData, projectsData } from "./data.js";
+import { UI } from "./ui.js";
+
 let footer = document.getElementsByTagName("footer")[0];
 
 let footerDate = footer.getElementsByClassName("footerDate")[0];
 footerDate.innerHTML = new Date().getFullYear();
 
-// document.addEventListener("DOMContentLoaded", () => {});
+class Portfolio {
+  constructor() {
+    this.ui = new UI();
+  }
+
+  init() {
+    try {
+      this.ui.init();
+
+      this.ui.renderProjectCards(projectsData);
+      this.ui.renderCertificateCards(certificationsData);
+    } catch (error) {
+      console.error("Failed to initialize app: ", error);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new Portfolio();
+  app.init();
+});
