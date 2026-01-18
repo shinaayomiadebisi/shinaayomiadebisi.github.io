2fed2c91e1259313a3be72a29d21bbd2dcfa0a44
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-01-18 04:13:31 -0800

    updated data

--- js/data.js
+++ js/data.js
@@ -2,55 +2,82 @@ const projectsData = [
   {
     id: 1,
     title: "School Management Application",
-    description: "School",
-    image: "",
+    description:
+      "School management application at allow access different users of admins, parents, teachers, and students",
+    image: "../assets/projectscreenshots/screen.png",
     githubLink: "",
     liveUrl: "",
-    tools: [],
+    tools: [
+      { name: "Javascript", class: "fa-js" },
+      { name: "Typescript", class: "fa-ts" },
+    ],
   },
   {
     id: 2,
     title: "MERN Real Estate",
     description: "Estate",
-    image: "",
+    image: "../assets/projectscreenshots/real-estate.png",
     githubLink: "",
-    liveUrl: "",
-    tools: [],
+    liveUrl: "https://mern-estate-hujk.onrender.com",
+    tools: [{ name: "Javascript", class: "fa-js" }],
   },
   {
     id: 3,
-    title: "",
+    title: "Amazon Clone",
     description: "",
-    image: "",
+    image: "../assets/projectscreenshots/amazon-clone.png",
     githubLink: "",
-    liveUrl: "",
-    tools: [],
+    liveUrl: "https://amazon-first-clone-me.netlify.app",
+    tools: [{ name: "Javascript", class: "fa-js" }],
   },
   {
     id: 4,
-    title: "",
+    title: "Weather App",
+    description: "",
+    image: "../assets/projectscreenshots/weather-app.png",
+    githubLink: "",
+    liveUrl: "https://shina-weather-app.netlify.app",
+    tools: [{ name: "Javascript", class: "fa-js" }],
+  },
+  {
+    id: 5,
+    title: "Fire Extinguisher",
     description: "",
     image: "",
     githubLink: "",
-    liveUrl: "",
-    tools: [],
+    liveUrl: "https://fire-extinguisher-rent-center-nuxt.netlify.app",
+    tools: [{ name: "nuxt", class: "" }],
   },
 ];
 
 const certificationsData = [
   {
     id: 1,
-    title: "C# Beginner Certification",
-    image: "",
-    certificateSiteUrl:
-      "https://www.udemy.com/certificate/UC-e8d17b9e-7b38-4a09-8cee-ba6986b08616/",
-  },
-  {
-    id: 2,
     title: "Fullstack MERN Real Estate",
     image: "",
     certificateSiteUrl:
       "https://www.udemy.com/certificate/UC-37c0f482-4aca-4684-b31c-2685df5ebfeb/",
+  },
+  {
+    id: 2,
+    title: "C# Beginner Certification",
+    image: "",
+    certificateSiteUrl:
+      "https://www.udemy.com/certificate/UC-e8d17b9e-7b38-4a09-8cee-ba6986b08616",
+  },
+  {
+    id: 3,
+    title: "C# Intermediate Certification",
+    image: "",
+    certificateSiteUrl:
+      "https://www.udemy.com/certificate/UC-5aa8e439-753a-4d61-8bf5-a06f31bfbf97",
+  },
+  {
+    id: 4,
+    title: "C# Intermediate Certification",
+    image: "",
+    certificateSiteUrl:
+      "https://www.udemy.com/certificate/UC-9c903acb-4367-4846-8c69-aba7e38f3ee2",
   },
 ];
 
