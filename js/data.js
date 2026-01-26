890c42aa87b10ab217355201d100d9da7904e3de
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-01-26 01:52:55 -0800

    added live link

--- js/data.js
+++ js/data.js
@@ -70,14 +70,14 @@ const certificationsData = [
   {
     id: 3,
     title: "C# Beginner Certification",
-    image: "..assets/certificates/C#-Beginner.jpg",
+    image: "../assets/certificates/C#-Beginner.jpg",
     certificateSiteUrl:
       "https://www.udemy.com/certificate/UC-e8d17b9e-7b38-4a09-8cee-ba6986b08616",
   },
   {
     id: 4,
     title: "C# Intermediate Certification",
-    image: "..assets/certificates/C#-Intermediate.jpg",
+    image: "../assets/certificates/C#-Intermediate.jpg",
     certificateSiteUrl:
       "https://www.udemy.com/certificate/UC-5aa8e439-753a-4d61-8bf5-a06f31bfbf97",
   },
