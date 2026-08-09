4ec962000dedef6a32cdecdeb5528baff0b6ec89
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-08-08 20:23:13 -0800

    Update

---
+++ test.js
@@ -0,0 +1,11 @@
+function reverseWords() {
+  let words = "Hello World".split("");
+  let newWords = "";
+  for (let i = words.length - 1; i >= 0; i--) {
+    newWords += words[i];
+  }
+
+  return newWords;
+}
+reverseWords();
+console.log(reverseWords());
